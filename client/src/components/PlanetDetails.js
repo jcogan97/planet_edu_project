56709be93d7bcc698c0b6a6a00fbc7859f456f04
Jack Cogan <jackcogan97@y7mail.com>
2022-01-15 06:53:52 +0000

    Edited: Card Styling

--- client/src/components/PlanetDetails.js
+++ client/src/components/PlanetDetails.js
@@ -10,7 +10,11 @@ const PlanetDetails = ({ planet }) => {
         </div>
         <p>Postition from sun: {planet.position_from_sun}</p>
         <p>Number of Moons: {planet.number_of_moons}</p>
-        <p>Notable Moons: {planet.notable_moons.map((moon) => moon + ', ')}</p>
+        <p>Notable Moons: {planet.notable_moons.map((moon, index) => 
+          index !== planet.notable_moons.length - 1
+          ? moon + ', '
+          : moon
+        )}</p>
         <p>Number of Known Rings: {planet.known_rings}</p>
         <p>Average Surface Temperature (Celcius): {planet.average_surface_temperature_c}</p>
         <p>Equatorial Circumference: {planet.equatorial_circumference_km}</p>
