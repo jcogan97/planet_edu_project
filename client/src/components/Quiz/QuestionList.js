959e57f60565cb616c49ff58b35a1bb3020d706b
NaomiValuks <naomi@valuks.com>
2022-01-16 22:25:37 +0000

    the quiz logic is now working

--- client/src/components/Quiz/QuestionList.js
+++ client/src/components/Quiz/QuestionList.js
@@ -3,29 +3,32 @@ import QuestionItem from "./QuestionItem";
 
 const QuestionList = ({questions}) => {
     
-    const [result, setResult] = useState({})
+    const [number, setNumber] = useState(0)
+    
+
+    const results = []
 
 
-
+    
 
     const listOfQuestions = questions.map((question, index) => {
-        return <QuestionItem question={question.question} answers={question.answers} correctAnswer={question.correctAnswer} index={index} result={result} setResult={setResult}/>
+        return <QuestionItem question={question.question} answers={question.answers} correctAnswer={question.correctAnswer} index={index} results={results} />
         
     });
 
     const handleSubmit = (event) => {
 		event.preventDefault();
-        // const data = {result};
-        // const json = JSON.stringify(data, null, 4);
-		// console.log(json);
-        console.log(result);
+        const numberOfCorrectAnswers = results.length
+        setNumber(numberOfCorrectAnswers);
     };
+
     
     return(
         <>
             <form onSubmit={handleSubmit}>
                 {listOfQuestions}
                 <input type="submit"/>
+                <p id="text-is-evil">you got {number} right!</p>
             </form>
         </>
     );
