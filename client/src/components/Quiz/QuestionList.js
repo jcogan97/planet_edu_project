46330a166edce06af8a4d540d6826c11b16766dd
NaomiValuks <naomi@valuks.com>
2022-01-16 15:06:24 +0000

    got data coming through now

--- client/src/components/Quiz/QuestionList.js
+++ client/src/components/Quiz/QuestionList.js
@@ -1,26 +1,32 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import QuestionItem from "./QuestionItem";
 
 const QuestionList = ({questions}) => {
     
-    const [formData, setformData] = useState({});
+    const [result, setResult] = useState({})
 
-    const listOfQuestions = questions.map((question) => {
-        return <QuestionItem question={question.question} answers={question.answers} correctAnswer={question.correctAnswer}/>
+
+
+
+    const listOfQuestions = questions.map((question, index) => {
+        return <QuestionItem question={question.question} answers={question.answers} correctAnswer={question.correctAnswer} index={index} result={result} setResult={setResult}/>
         
     });
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        console.log(event);
-    }
+    const handleSubmit = (event) => {
+		event.preventDefault();
+        // const data = {result};
+        // const json = JSON.stringify(data, null, 4);
+		// console.log(json);
+        console.log(result);
+    };
     
     return(
         <>
-        <form onSubmit={onSubmit}>
-            {listOfQuestions}
-            <input type="submit"/>
-        </form>
+            <form onSubmit={handleSubmit}>
+                {listOfQuestions}
+                <input type="submit"/>
+            </form>
         </>
     );
 }
