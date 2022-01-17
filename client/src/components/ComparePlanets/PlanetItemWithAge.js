2ef8ebc3ec58de759e1a17b2b27652455aad62a4
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-17 11:44:38 +0000

    Age calculator submit styling updated, age displays are now blank before input, Earth age returns as round integer

--- client/src/components/ComparePlanets/PlanetItemWithAge.js
+++ client/src/components/ComparePlanets/PlanetItemWithAge.js
@@ -4,24 +4,40 @@ import './ComparePlanet.css';
 const PlanetItemWithAge = ({planet, age}) => {
 
   const getAges = (planet, age) => {
+    let ageRelative = 0
+
+    if (planet.name === "Earth") {
+      return age
+    }
+    else {
+      const ageInt = parseInt(age)
+      ageRelative = (ageInt / planet.orbital_period).toFixed(2);
+    }
     
-    const ageInt = parseInt(age)
-    const ageRelative = (ageInt / planet.orbital_period).toFixed(2);
+    
     
     return ageRelative;
     
-}
+  }
 
 const relativeAge = getAges(planet, age);
-  
-      return (
-        <>
-        <img className='scalable drop-shadow' id={ planet.name } src={ planet.image } alt={ planet.name } />
-        <p id="display-text">{relativeAge} {planet.name} years</p>
+  if (age > 0) {
+    return (
+      <>
+          <img className='scalable drop-shadow' id={ planet.name } src={ planet.image } alt={ planet.name } />
         
-        
-        </>
-      );
+          <p id="display-text">{relativeAge} {planet.name} years</p>
+          </>
+    )
   }
+  else {
+    return(
+      <img className='scalable drop-shadow' id={ planet.name } src={ planet.image } alt={ planet.name } />
+    )
+    
+
+  }
+  }
+    
    
   export default PlanetItemWithAge;
