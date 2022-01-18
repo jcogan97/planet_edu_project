f01f9fff043c4c97285d81e6240a67454e4b09e4
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-18 17:12:36 +0000

    Styling changes

--- client/src/components/ComparePlanets/DataForm.js
+++ client/src/components/ComparePlanets/DataForm.js
@@ -14,21 +14,29 @@ const DataForm = ({handleDateInput, handleWeightInput}) => {
     }
     
     return (
-        <div id="display-text">
+        <div id="info">
+        
+       
+            
            <form onSubmit={onSubmitDate}>
             <label>Please enter your age:</label>
             <br/>
             <input type="number" id="age" />
             <input type="submit" id="submit-button" value="Submit Age"></input>
         </form>
+        
+       
+        
+        
         <form onSubmit={onSubmitWeight}>
             <label>Or enter your weight (kg):</label>
             <br/>
             <input type="number" id="weight" />
             <input type="submit" id="submit-button" value="Submit Weight"></input>
         </form>
-            
-            
+        
+        
+        
             
         </div>
     )
